4adb988abc1ee7681a68ba8df6229814295c0fc0
alexcastillla <alexander.zcas@gmail.com>
2020-11-21 13:40:42 +0000

    Modified db and added seed files

--- src/main.py
+++ src/main.py
@@ -8,36 +8,44 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, Users
+
+from models import db
+from init_database import init_db
 #from models import Person
 
 app = Flask(__name__)
+app.app_context().push()
+data_base = os.environ['DB_CONNECTION_STRING']
+
 app.url_map.strict_slashes = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
+app.config['SQLALCHEMY_DATABASE_URI'] = data_base
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 MIGRATE = Migrate(app, db)
 db.init_app(app)
+
 CORS(app)
 setup_admin(app)
+app.cli.add_command(init_db)
 
-# Handle/serialize errors like a JSON object
-@app.errorhandler(APIException)
-def handle_invalid_usage(error):
-    return jsonify(error.to_dict()), error.status_code
+# # Handle/serialize errors like a JSON object
+# @app.errorhandler(APIException)
+# def handle_invalid_usage(error):
+#     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
-@app.route('/')
-def sitemap():
-    return generate_sitemap(app)
+# # generate sitemap with all your endpoints
+# @app.route('/')
+# def sitemap():
+#     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# @app.route('/user', methods=['GET'])
+# def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+#     response_body = {
+#         "msg": "Hello, this is your GET /user response "
+#     }
 
-    return jsonify(response_body), 200
+#     return jsonify(response_body), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -5,32 +5,76 @@ db = SQLAlchemy()
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(45), unique=True, nullable=False)
-    email = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), unique=False, nullable=False)
     location = db.Column(db.String(30), unique=False, nullable=False)
-    photo = db.Column(db.String(250), unique=False, nullable=True)
+    photo = db.Column(db.String(255), unique=False, nullable=True)
     users_room_relationship = db.relationship('Room', lazy=True)
     users_friends_relationship = db.relationship('Friends', lazy=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "location": self.location,
+            "photo": self.photo
+        }
     
 class Room(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name_room = db.Column(db.String(30), unique=False, nullable=False)
+    name_room = db.Column(db.String(20), unique=False, nullable=False)
     id_user = db.Column(db.String(45), db.ForeignKey("users.username"),nullable=False)
     room_Plants_relationship = db.relationship('Plants', lazy=True)
+
+    def __repr__(self):
+        return '<Room %r>' % self.name_room
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name_room": self.name_room,
+            "id_user": self.id_user
+        }
 
 class Plants(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name_plant = db.Column(db.String(45), unique=False, nullable=False)
-    tipo_plant = db.Column(db.String(45), unique=True, nullable=False)
+    tipo_plant = db.Column(db.Enum("Interior", "Exterior"), unique=True, nullable=False)
     id_room = db.Column(db.Integer, db.ForeignKey("room.id"), nullable=False)
     users_Plants_Tipo_relationship = db.relationship('Plants_Tipo', lazy=True)
     users_Plants_Time_relationship = db.relationship('Plants_Time', lazy=True)
+
+    def __repr__(self):
+        return '<Plants %r>' % self.id_room
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username_plant": self.name_plant,
+            "tipo_plant": self.tipo_plant,
+            "id_room": self.id_room
+        }
 
 class Plants_Tipo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name_tipo = db.Column(db.String(45), db.ForeignKey("plants.tipo_plant"), nullable=False)
     humidity_ideal = db.Column(db.Integer, unique=False, nullable=False)
     temperature_ideal = db.Column(db.Integer, unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Plants_Tipo %r>' % self.name_tipo
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name_tipo": self.name_tipo,
+            "humidity_ideal": self.humidity_ideal,
+            "temperature_ideal": self.temperature_ideal
+        }
 
 class Plants_Time(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -40,17 +84,29 @@ class Plants_Time(db.Model):
     sensor_name = db.Column(db.String(45), unique=False, nullable=False)
     id_plant_name = db.Column(db.Integer, db.ForeignKey("plants.id"),nullable=False)
 
+    def __repr__(self):
+        return '<Plants_Time %r>' % self.id_plant_name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "time_stamp": self.time_stamp,
+            "humidity_sensor": self.humidity_sensor,
+            "temperature_sensor": self.temperature_sensor,
+            "sensor_name": self.sensor_name
+        }
+
 class Friends(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     id_user = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     friend_name = db.Column(db.String(45), unique=False, nullable=False)
 
-    # def __repr__(self):
-    #     return '<User %r>' % self.username
+    def __repr__(self):
+        return '<Friends %r>' % self.id_user
 
-    # def serialize(self):
-    #     return {
-    #         "id": self.id,
-    #         "email": self.email,
-    #         # do not serialize the password, its a security breach
-    #     }
+    def serialize(self):
+        return {
+            "id": self.id,
+            "id_user": self.id_user,
+            "friend_name": self.friend_name
+        }
